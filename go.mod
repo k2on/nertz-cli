9effd43b9283d34a5c03410a017916a60ee49751
Max Koon <max@koon.dev>
2022-06-21 21:36:09 -0400

    feat: draw from on key and background process

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/k2on/nertz
+
+go 1.18
+
+require golang.org/x/term v0.0.0-20220526004731-065cf7ba2467
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.4.1-0.20210905002822-f057f0a857a1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
+
+require (
+	github.com/rivo/tview v0.0.0-20220610163003-691f46d6f500
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/rivo/tview"
+)
+
+type State struct {
+	started bool
+	key rune
+	i int
+}
+
+func makeLoop() func(*tview.Application,*tview.Box,*State) {
+
+return func(app *tview.Application, box *tview.Box, state *State) {
+	for {
+
+	state.i++
+
+		box.SetTitle("sdh")
+
+	time.Sleep(1 * time.Second)
+	}
+}
+
+}
+
+func main() {
+	box := tview.NewBox().SetBorder(true).SetTitle("nertz")
+	state := &State{}
+
+	app := tview.NewApplication().SetRoot(box, true)
+
+
+
+
+	start := func() {
+		for {
+		state.i++
+		app.QueueUpdateDraw(func() {
+
+		
+		box.SetTitle("hmmm " + strconv.Itoa(state.i) + string(state.key))
+		})
+		time.Sleep(1 * time.Second)
+		}
+
+	}
+	go start()
+
+	box.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		state.key = event.Rune()
+
+		box.SetTitle("hmmm " + strconv.Itoa(state.i) + string(state.key))
+
+		
+
+		return event
+	})
+
+
+	if err := app.Run(); err != nil {
+		panic(err)
+	}
+
+}
